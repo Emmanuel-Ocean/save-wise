4283946ed2f08cf573e0c8e60e3184b4668249b4
Emmanuel O. Olukanni <oceanemm18@gmail.com>
2021-06-29 13:04:57 +0100

    api commit3

--- savenow/views.py
+++ savenow/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-from .models import *
 
 # Create your views here.
 def index(request):
-  if request.method == 'POST':
-    Savenow.objects.create(
-      name=request.POST['name'],
-      amount=request.POST['amount'],
-      purpose=request.POST['purpose'],
-      saved_at=request.POST['saved_at'],
-      )
-  context = {}
-  return render(request, 'index.html', context)
+  context = {
+    
+
+  }
+  return render(request,'index.html',context)
 
 def myplan(request):
   context = {
