c1ea3f05757e42ecaceb0b1f582d9542bb62fce2
Emmanuel O. Olukanni <oceanemm18@gmail.com>
2021-06-29 12:54:07 +0100

    api commit2

--- savenow/models.py
+++ savenow/models.py
@@ -5,7 +5,7 @@ class Savenow(models.Model):
     name = models.CharField(max_length=200, default=False, null=True)
     amount = models.DecimalField(max_digits=20,decimal_places=5, default=False, null=True)
     purpose = models.CharField(max_length=200, default=False, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    saved_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
--- savenow/views.py
+++ savenow/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-  context = {
-
-  }
-  return render(request,'index.html',context)
+  if request.method == 'POST':
+    Savenow.objects.create(
+      name=request.POST['name'],
+      amount=request.POST['amount'],
+      purpose=request.POST['purpose'],
+      saved_at=request.POST['saved_at'],
+      )
+  context = {}
+  return render(request, 'index.html', context)
 
 def myplan(request):
   context = {
